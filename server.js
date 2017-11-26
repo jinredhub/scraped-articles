623aab0418a23250c8d0882ce02654fabfea6139
jinredhub <jin@nieblo.com>
2017-11-25 21:26:50 -0800

    update heroku env

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-// var PORT = 3000;
+var PORT =  3000;
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb//localhost/mongoHeadlines";
-MONGODB_URI = mongoose.connect;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+ mongoose.connect = MONGODB_URI;
 
 // Initialize Express
 var app = express();
