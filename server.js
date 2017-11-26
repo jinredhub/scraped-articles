d5c21c5c01d3d6746ab5e572a7a559e3c1252cae
jinredhub <jin@nieblo.com>
2017-11-25 23:41:13 -0800

    testing env

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var PORT =  3000 || process.env.PORT;
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
  // mongoose.connect = MONGODB_URI;
- MONGODB_URI = mongoose.connect;
+ // MONGODB_URI = mongoose.connect;
 
 // Initialize Express
 var app = express();
