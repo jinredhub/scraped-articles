a5a000eaabd0732edff40e51c199978a5cebd01f
jinredhub <jin@nieblo.com>
2017-11-25 21:00:34 -0800

    adding heroku mongodb environment

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+// var PORT = 3000;
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb//localhost/mongoHeadlines";
+MONGODB_URI = mongoose.connect;
 
 // Initialize Express
 var app = express();
